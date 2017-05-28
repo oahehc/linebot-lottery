f52717c6c2d98e9ab0c6b860489326acf00f4b2a
oahehc <oahehc@gmail.com>
2017-05-28 19:30:05 +0800

    add typescript setting

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # linebot-lottery
 collect lottery information and auto alert by using line message
+
+
+### Scripts
+```bash
+webpack       # compile index.ts to dist/index.js
+yarn start    # execute dist/index.js
+yarn test     # compile then execute dist/index.js
+```
+
+
+# TODO
+1. LineBot link to lambda function
+2. collect website information by lambda function
+3. push line msg by lambda function
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: path.join(__dirname, 'index.ts')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        filename: '[name].js',
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            loader: 'ts-loader'
+        }]
+    },
+}
